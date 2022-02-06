f8628267aedfcc5f267e4967dba10b62c0fbd2a8
Janaki2425 <96913266+Janaki2425@users.noreply.github.com>
2022-02-06 16:57:57 +0530

    main program

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace CabInvoiceGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to cab invoice Generator");
+            InvoiceGenerator generator = new InvoiceGenerator(RideType.NORMAL);
+            double fare = generator.CalculateFare(2.0, 5);
+            Console.WriteLine(fare);
+        }
+
+
+    }
+}
