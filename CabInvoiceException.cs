1a86f92806fd8dc7e5264904e809e83f499801d2
Janaki2425 <96913266+Janaki2425@users.noreply.github.com>
2022-02-06 17:11:11 +0530

    cab invoice exception

---
+++ CabInvoiceException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabInvoiceGenerator
+{
+    public class CabInvoiceException : Exception
+    {
+        public enum ExceptionType
+        {
+            Invalid_ride_type,
+            Invalid_distance,
+            Invalid_time,
+            Null_rides,
+            Invalid_user_id
+        }
+
+        ExceptionType type;
+        public CabInvoiceException(ExceptionType type, string message) : base(message)
+        {
+            this.type = type;
+        }
+    }
+
+}
